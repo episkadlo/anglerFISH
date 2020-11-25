d80dc8ca66fcc383992186eb3ae68bd33ffa08ae
Bastian Eichenberger <beichenberger@57-143-16-94.dyn.cable.fcom.ch>
2020-11-25 17:31:50 +0100

    pythonify customBed to follow black code style

--- helperScripts/customBed2Fasta.py
+++ helperScripts/customBed2Fasta.py
@@ -1,42 +1,60 @@
 #!/usr/bin/env python
-scriptName = 'customBed2Fasta'
+import pandas
+import argparse
+
+scriptName = "customBed2Fasta"
 Version = 1.2
 
 
-import pandas
-import os
-import argparse
+def bed_to_fasta(file, out):
+    """Convert .bed file to .fasta file.
+
+    Loops over the rows of input bed file to extract the sequence
+    alone from the seq column and save it to a new fasta file.
+    """
+    df = pandas.read_csv(
+        str(file), sep="\t", header=None, columns=["chr", "pos1", "pos2", "seq", "Tm"]
+    )
+    probe_inx = [f">probe_{i}_{out}" for i in range(1, len(df) + 1)]
+
+    new_inx = pandas.Series(probe_inx)
+    out_df = pandas.concat([new_inx, df["seq"]], axis=1, sort=False)
+
+    with open(str(out) + ".fasta", "w+") as f:
+        for index, row in out_df.iterrows():
+            f.write(str(row[0]) + "\n")
+            f.write(str(row["seq"]) + "\n")
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description="Extract sequences from .bed file and save them as .fasta."
+    )
+    parser.add_argument(
+        "-f",
+        "--File",
+        required=True,
+        help=(
+            "Input bed file, from which the single sequences will be extracted "
+            "and saved as a fasta file. [required]"
+        ),
+    )
+    parser.add_argument(
+        "-o", "--out", required=True, help="Output fasta file name. [required]"
+    )
+    args = parser.parse_args()
+    return args
+
 
 def main():
-
-    #loop over the rows of input bed file in order to extract the sequence alone from the seq column and save it to the new file file
-    def bedToFasta(file, out):
-            df = pandas.read_csv(str(file), sep = '\t', header = None)
-            df.columns= ['chr', 'pos1', 'pos2', 'seq', 'Tm']
-            probe_inx = []
-            for i in range (1, len(df)+1): probe_inx.append('>probe_' + str(i) + "_" + str(out))
-            new_inx = pandas.Series(probe_inx)
-            out_df = pandas.concat([new_inx, df['seq']], axis = 1, sort = False)
-            f= open(str(out) + '.fasta','w+')
-            for index, row in out_df.iterrows():
-                f.write(str(row[0])+"\n")
-                f.write(str(row['seq'])+"\n")
-            f.close()
-
-
-    #Initialize parse_args to get input file
-    parser = argparse.ArgumentParser(description="Extracts sequences from .bed file and saves them as a single .fasta file")
-
-    # add ArgumentParser
-    parser.add_argument("-f", "--File", required=True, help = "input bed file, from which the single sequences will be extracted and saved as a fasta file; reqired")
-    parser.add_argument("-o", "--out", required=True, help = "output fasta file name; required")
-    args = parser.parse_args()
+    # Parse user arguments
+    args = _parse_args()
     inFile = args.File
     outSuffix = args.out
 
+    # Call bed_to_fasta to extract sequences
+    bed_to_fasta(inFile, outSuffix)
 
-    #call the bedToFasta on the user's input file to extract sequences
-    bedToFasta(inFile, outSuffix)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
